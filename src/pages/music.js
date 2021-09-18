2d682a528b8d84be2693a739897e35b7e7f68a54
David Wang <62212652+enjineerMan@users.noreply.github.com>
2021-09-18 19:15:29 -0400

    abc format string has correct note pitches and duration. Still need to implement rests, and add '|\' at the end of every measure

--- src/pages/music.js
+++ src/pages/music.js
@@ -54,31 +54,50 @@ class MusicPage extends Component {
           key_sig = key_sig.substring(0,2)+key_sig.substring(4);
         }
         console.log(key_sig);
+        var time_sig = beats_per_bar.toString()+"/4";
+        var abc_string = 
+        `X: 1
+        T: your sheet
+        M: ${time_sig}
+        L: 1/8
+        K: ${key_sig}
+        |`;
         for(var i = 0; i<melody.length; i++){
           var note_duration = melody[i]["duration"];
           var note_value = midi_to_note(Math.round(melody[i]["midi_pitch"])); //note name + octave
           var note_type;
+
+          var rest_type = 1 - note_type;
+          var octv = note_value[note_value.length-1];
+          var note_to_add = key_sig.length==6?note_value.substring(0,1) : note_value.substring(0,2);
+          if (octv==3){
+            note_to_add+=",";
+          }else if(octv==4){
+            //do nothing
+          }else if(octv==5){
+            note_to_add = note_to_add.toLowerCase();
+          }else if(octv==6){
+            note_to_add = note_to_add.toLowerCase()+"\'";
+          }
+
           if(Math.abs(note_duration/beat_duration - 1) < Math.abs(note_duration/beat_duration - 0.75)){ //quarter
             note_type = 1;
+            note_to_add += "4";
           }else if(Math.abs(note_duration/beat_duration - 0.75) < Math.abs(note_duration/beat_duration - 0.5)){ // doted eighth
             note_type = 0.75;
+            note_to_add += "3";
           }else if(Math.abs(note_duration/beat_duration - 0.5) < Math.abs(note_duration/beat_duration - 0.25)){ //eigth
             note_type = 0.5;
+            note_to_add += "2";
           }else if(Math.abs(note_duration/beat_duration - 0.25) < Math.abs(note_duration/beat_duration)){ //sixteenth
             note_type = 0.25;
+            note_to_add += "1";
           }else{
             note_type = 0;
           }
 
-          var rest_type = 1 - note_type;
-          var time_sig = beats_per_bar.toString()+"/4";
-          var abc_string = 
-          `X: 1
-          T: your sheet
-          M: ${time_sig}
-          K: ${key_sig}
-          |`;
-          notes.push({"note": note_value, "note_type": note_type, "rest_type": rest_type});
+          abc_string +=  note_to_add; // need to add "|\" at the end of every measure. Also need to implement rests
+          notes.push({"note": note_value, "note_type": note_type});
         }
         console.log(notes);
         //console.log(midi_notes);
