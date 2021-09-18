257b8419340347e5d1e989bca8f0d57fb4d20522
David Wang <62212652+enjineerMan@users.noreply.github.com>
2021-09-18 12:58:14 -0400

    midi values to note + octave conversion done

--- src/App.js
+++ src/App.js
@@ -1,58 +1,6 @@
 import './App.css';
 import MusicPage from './pages/music.js';
 
-var accessId = 'f5eeb248-839b-4ac3-8fa3-a557e5c78a0e';
-var taskUrl = 'process/reverb';
-var parameters = { blocking: false, format: 'json', access_id: accessId };
-
-// the values for these parameters were taken from the corresponding controls in the demo form
-parameters['input_file'] = 'http://www.sonicAPI.com/music/solo_sax.mp3';
-parameters['preset'] = 'medium_hall';
-parameters['wetness'] = '0.2';
-    
-function onTaskStarted(data) {
-    var fileId = data.file.file_id;
-    
-    // request task progress every 500ms
-    var polling = setInterval(pollTaskProgress, 500);
-   
-    function pollTaskProgress() {
-        $.ajax({ url: 'https://api.sonicAPI.com/file/status?file_id=' + fileId + '&access_id=' + accessId + '&format=json', 
-                 crossDomain: true, success: function(data) {
-            if (data.file.status == 'ready') {
-                onTaskSucceeded(fileId);
-                clearInterval(polling);
-            } else if (data.file.status == 'working') {
-                $('#result').text(data.file.progress + '% done');
-            }
-        }});
-    }
-}
-
-function onTaskSucceeded(fileId) {
-    // create HTML5 audio player
-    var downloadUrl = 'https://api.sonicAPI.com/file/download?file_id=' + fileId + '&access_id=' + accessId + '&format=mp3-cbr';
-    var audio = '<audio src="' + downloadUrl + '" controls="controls" autoplay="autoplay">';
-    
-    $('#result').html(audio);
-}
-
-function onTaskFailed(response) {
-    var data = $.parseJSON(response.responseText);
-    var errorMessages = data.errors.map(function(error) { return error.message; });
- 
-    $('#result').text('Task failed, reason: ' + errorMessages.join(','));
-}
-
-// start task when clicking on the "Start task" button
-$(document).ready(function() {
-    $('#start').click(function() {
-    	// execute an HTTP GET using the task's URL, the parameters and callback functions defined above
-        $.ajax({ url: 'https://api.sonicAPI.com/' + taskUrl, data: parameters, 
-                 success: onTaskStarted, error: onTaskFailed, crossDomain: true });
-    });
-});
-
 // some code that uses the library
 function App() {
   return (
